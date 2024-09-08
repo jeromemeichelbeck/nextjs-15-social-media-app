1010a9d27b0d688522888b5ddc52576f0a4d9922
Jérôme Meichelbeck <jerome@meichelbeck.io>
2024-09-08 18:23:37 +0200

    Add auth layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import { validateRequest } from "@/auth";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function AuthLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const { user } = await validateRequest();
+
+  if (user !== null) {
+    redirect("/");
+  }
+
+  return <>{children}</>;
+}

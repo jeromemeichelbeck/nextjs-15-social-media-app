5cf06f48ab5ad7a0ed7924523f40373cd72f9640
Jérôme Meichelbeck <jerome@meichelbeck.io>
2024-09-10 13:08:02 +0200

    Display posts on home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,23 @@
 import { validateRequest } from "@/auth";
 import PostEditor from "@/components/posts/editor/post-editor";
+import Post from "@/components/posts/post";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 
 export default async function Home() {
-  const { user } = await validateRequest();
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
-    <main className="w-full">
-      <PostEditor />
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
     </main>
   );
 }

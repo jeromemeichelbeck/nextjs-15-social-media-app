9e8e7d3707d187141ec2fe8dd179a3a410498ae7
Jérôme Meichelbeck <jerome@meichelbeck.io>
2024-09-18 14:20:16 +0200

    Add main not found page

---
+++ src/app/(main)/not-found.tsx
@@ -0,0 +1,8 @@
+export default function MainNotFount() {
+  return (
+    <main className="my-12 w-full space-y-3 text-center">
+      <h3 className="text-3xl font-bold">Not Found</h3>
+      <p>The page you are looking for does not exist</p>
+    </main>
+  );
+}

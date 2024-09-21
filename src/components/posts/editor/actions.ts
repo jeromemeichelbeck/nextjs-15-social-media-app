db185f7e501d5570f516e6a22f3c02b5dcd85029
Jérôme Meichelbeck <jerome@meichelbeck.io>
2024-09-21 16:51:00 +0200

    Add use media uplaod hook

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -12,10 +12,16 @@ export async function submitPost(post: CreatePostValues) {
     throw Error("Unauthorized");
   }
 
-  const { content } = createPostSchema.parse(post);
+  const { content, mediaIds } = createPostSchema.parse(post);
 
   return prisma.post.create({
-    data: { content, authorId: user.id },
+    data: {
+      content,
+      authorId: user.id,
+      attachments: {
+        connect: mediaIds.map((mediaId) => ({ id: mediaId })),
+      },
+    },
     include: getPostDataInclude(user.id),
   });
 }
---
+++ src/components/posts/editor/useMediaUpload.ts
@@ -0,0 +1,110 @@
+import { useToast } from "@/hooks/use-toast";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useState } from "react";
+
+export interface Attachment {
+  file: File;
+  mediaId?: string;
+  isUploading: boolean;
+}
+
+export default function useMediaUpload() {
+  const { toast } = useToast();
+
+  const [attachments, setAttachements] = useState<Attachment[]>([]);
+  const [uploadProgress, setUploadProgress] = useState<number>();
+
+  const { startUpload, isUploading } = useUploadThing("attachment", {
+    onBeforeUploadBegin(files) {
+      const renamedFiles = files.map((file) => {
+        const [_, extension] = file.name.split("0");
+
+        return new File(
+          [file],
+          `attachment_${crypto.randomUUID()}.${extension}`,
+          { type: file.type },
+        );
+      });
+
+      setAttachements((previouAttachments) => [
+        ...previouAttachments,
+        ...renamedFiles.map((file) => ({ file, isUploading: true })),
+      ]);
+
+      return renamedFiles;
+    },
+    onUploadProgress: setUploadProgress,
+    onClientUploadComplete(results) {
+      setAttachements((previouAttachments) =>
+        previouAttachments.map((attachment) => {
+          const uploadResult = results.find(
+            (result) => result.name === attachment.file.name,
+          );
+
+          if (!uploadResult) {
+            return attachment;
+          }
+
+          return {
+            ...attachment,
+            mediaId: uploadResult.serverData.mediaId,
+            isUploading: false,
+          };
+        }),
+      );
+    },
+    onUploadError(error) {
+      setAttachements((previousAttachements) =>
+        previousAttachements.filter((attachment) => !attachment.isUploading),
+      );
+
+      toast({
+        variant: "destructive",
+        description: error.message,
+      });
+    },
+  });
+
+  function handleStartUpload(files: File[]) {
+    if (isUploading) {
+      toast({
+        variant: "destructive",
+        description: "Please wait for the current upload to finish",
+      });
+
+      return;
+    }
+
+    if (attachments.length + files.length > 5) {
+      toast({
+        variant: "destructive",
+        description: "You can upload up to 5 attachments per post",
+      });
+      return;
+    }
+
+    startUpload(files);
+  }
+
+  function removeAttachment(fileName: string) {
+    setAttachements((previousAttachments) =>
+      previousAttachments.filter(
+        (attachment) => attachment.file.name !== fileName,
+      ),
+    );
+  }
+
+  function reset() {
+    setAttachements([]);
+    setUploadProgress(undefined);
+  }
+
+  return {
+    startUpload: handleStartUpload,
+    attachments,
+    isUploading,
+    uploadProgress,
+    removeAttachment,
+    reset,
+  };
+}
